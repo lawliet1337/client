9a1c2a5064671804cfa2de8769383163b6dbc9eb
wesley <wesdasilvalol@gmail.com>
2022-03-09 04:23:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [values, setValues] = useState();
+  const handleChangeValues = (value) => {
+    setValues((prevValue) => ({
+      ...prevValue,
+      [value.target.name] : value.target.value,
+    }));
+  };
+
+  const handleClickButton = () => {
+    console.log(values);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app--container">
+      <div className="register--container">
+        <h1 className="register--title">Shop</h1>
+        <input type="text" name="name" placeholder="Nome" className="register--input" onChange={handleChangeValues}/>
+        <input type="text" name="cost" placeholder="Preço" className="register--input" onChange={handleChangeValues}/>
+        <input type="text" name="category" placeholder="Categoria" className="register--input" onChange={handleChangeValues}/>
+        <button className="register--button" onClick={()=> handleClickButton()}>Cadastrar</button>
+      </div>
     </div>
   );
 }
